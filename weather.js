64be04fd0f73d9eadc7d13b3fd6d068e171521f5
RSakultarawattn <ray.sakultarawattn@gmail.com>
2020-11-10 13:36:35 -0800

    weath

--- weather.js
+++ weather.js
@@ -1,12 +1,12 @@
 function weatherMunge(weatherData) {
 
-    let weatherArray = weatherData.data.map(item => 
-  return {
+    let weatherArray = weatherData.data.map(item => {
+        return {
 
-        forecast: item.weather.description,
-        time: item.datetime
+            forecast: item.weather.description,
+            time: item.datetime
 
-    };
+        };
     });
     return weatherArray.slice(0, 8);
 }
